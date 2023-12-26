710d824b4d664da00292adbb3df3e484834fb1a1
Göktuğ Ertekin <130211667+GoktugErtekin@users.noreply.github.com>
2023-12-26 22:43:07 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let scissors = document.querySelector('.scissors');
+let rock = document.querySelector('.rock');
+let paper = document.querySelector('.paper');
+let spock = document.querySelector('.spock');
+let lizard = document.querySelector('.lizard');
+
+const selected = () => {
+    
+}
+
+scissors.addEventListener("click", selected);
